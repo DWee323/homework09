352e55c83fbb30e58e2fcf614658b0806da4e22e
Diane Wee <hwee1@id.iit.edu>
2018-10-30 15:08:36 -0500

    copied hw prompt and html, css

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+The following tasks do not need to be done in order. I will review the final file.
+
+The final file should just the code, no console.log calls, debug code, or commented out code unless notes to me.
+
+Review the ***** Canvas API documents on MDN *****.
+
+Take time and be efficient with your code.
+
+Create a index.html and script.js like we did in class.
+
+The following code will be in your script.js file.
+
+Using the Canvas API, draw the following image.
+*/
