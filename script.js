24e670093fb5dd38092a72ffb84586c10d2527d2
Diane Wee <hwee1@id.iit.edu>
2018-11-02 07:45:54 -0500

    face is made - gotta clean up the code

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const circle = (x,y,radius) => {
 
 //face outline
 ctx.beginPath();
-circle(250, 250, 230);
+circle(250, 250, 230); //lowest point of face is (250,480): to match tongue bottom
 ctx.fillStyle = "#ffde67"; //off mustard
 ctx.fill();
 
@@ -40,25 +40,16 @@ circle(250 +95, 250 -60, 35);
 ctx.fillStyle = "#664d23";
 ctx.fill();
 
-//mouth
 //ctx.arcTo(x1, y1, x2, y2, radius); -> adds a circular arc to the current path. It is commonly used for making rounded corners.
 
-/*
-ctx.beginPath();
-ctx.moveTo(x1, y1); //start point
-ctx.arcTo(x2, y2, x3, y3, radius); //point where angle is made ~ end point
-ctx.lineTo(x3, y3); //end point
-ctx.stroke();
-*/
-
-//ctx.arc(x,y,radius,strtAngle,endAngle,(antiClockwise))
-
+//mouth, body
 ctx.beginPath();
 ctx.arc(250, 250 +30, 150, 0, Math.PI);//line is there, but invisible
 ctx.closePath(); //no closePath with rounding corner?
 ctx.fillStyle = "#664d23";
 ctx.fill();
 
+//mouth, top, rounded corners
 ctx.beginPath();
 ctx.moveTo(250 -150, 250 +30); //start point
 ctx.arcTo(250 -150, (250+30) -10, (250-150) +10, (250+30) -10, 10); //point where angle is made ~ end point
@@ -66,4 +57,35 @@ ctx.lineTo(250+150 -10, (250+30) -10); //end point
 ctx.arcTo(250+150, (250+30) -10, (250+150), (250+30) +10, 10);
 ctx.closePath();
 ctx.fillStyle = "#664d23";
+ctx.fill();
+
+//tongue! challenge, idontwannaacceptiwannacryandnotdiebutsleepdamnit
+//tongue, bottom round part
+ctx.beginPath();
+ctx.arc(250, 480-80, 80, 0, Math.PI);//line is there, but invisible
+ctx.closePath(); //no closePath with rounding corner?
+ctx.fillStyle = "#f86e7d"; //lighter pink
+ctx.fill();
+
+//tongue, upper rectangular, rounded corners
+ctx.beginPath();
+ctx.moveTo(250 -80, 400); //start point
+ctx.lineTo(250 -80, 320 +10);
+ctx.arcTo(250 -80, 320, 250-80 +10, 320, 10); 
+//point where angle is made ~ end point
+ctx.lineTo(250+80 -10, 320); //end point
+ctx.arcTo(250+80, 320, 250+80, 320+10, 10);
+ctx.lineTo(250+80, 400);
+ctx.closePath();
+ctx.fillStyle = "#f86e7d";
+ctx.fill();
+
+//tongue, center line (triangle)
+ctx.beginPath();
+ctx.moveTo(250-10, 320);
+ctx.lineTo(250+10, 320); 
+ctx.lineTo(250, 450);
+ctx.closePath();
+//ctx.lineTo(250-10, 320);
+ctx.fillStyle = "#e3586c";//darker pink
 ctx.fill();
