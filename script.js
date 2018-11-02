1137cb4b6ff2a0eef368bd8ee1998dbf3aed3001
Diane Wee <hwee1@id.iit.edu>
2018-11-02 07:53:35 -0500

    face is made - cleaned most of the code

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
-/*
-The following tasks do not need to be done in order. I will review the final file.
-
-The final file should just the code, no console.log calls, debug code, or commented out code unless notes to me.
-
-Review the ***** Canvas API documents on MDN *****.
-
-Take time and be efficient with your code.
-
-Create a index.html and script.js like we did in class.
-
-The following code will be in your script.js file.
-
-Using the Canvas API, draw the following image.
-*/
-
+//to Ali: many of the parameter values are kept as mathematic equation (e.g. 250-80+10, instead of 180) to conserve certain anchor points and values - were there more time, I would think of making variables or functions to store these values for easier modification and understanding of code, but I got to get to school now...
 
 let canvas = document.querySelector("#emoji");
 let ctx = canvas.getContext("2d");
@@ -40,40 +25,37 @@ circle(250 +95, 250 -60, 35);
 ctx.fillStyle = "#664d23";
 ctx.fill();
 
-//ctx.arcTo(x1, y1, x2, y2, radius); -> adds a circular arc to the current path. It is commonly used for making rounded corners.
-
 //mouth, body
 ctx.beginPath();
-ctx.arc(250, 250 +30, 150, 0, Math.PI);//line is there, but invisible
-ctx.closePath(); //no closePath with rounding corner?
+ctx.arc(250, 250 +30, 150, 0, Math.PI);
+ctx.closePath(); //Question: no closePath with rounding corner?
 ctx.fillStyle = "#664d23";
 ctx.fill();
 
 //mouth, top, rounded corners
+//ctx.arcTo(x1, y1, x2, y2, radius); -> adds a circular arc to the current path. It is commonly used for making rounded corners.
 ctx.beginPath();
-ctx.moveTo(250 -150, 250 +30); //start point
-ctx.arcTo(250 -150, (250+30) -10, (250-150) +10, (250+30) -10, 10); //point where angle is made ~ end point
-ctx.lineTo(250+150 -10, (250+30) -10); //end point
+ctx.moveTo(250 -150, 250 +30);
+ctx.arcTo(250 -150, (250+30) -10, (250-150) +10, (250+30) -10, 10);
+ctx.lineTo(250+150 -10, (250+30) -10);
 ctx.arcTo(250+150, (250+30) -10, (250+150), (250+30) +10, 10);
 ctx.closePath();
 ctx.fillStyle = "#664d23";
 ctx.fill();
 
-//tongue! challenge, idontwannaacceptiwannacryandnotdiebutsleepdamnit
 //tongue, bottom round part
 ctx.beginPath();
-ctx.arc(250, 480-80, 80, 0, Math.PI);//line is there, but invisible
-ctx.closePath(); //no closePath with rounding corner?
+ctx.arc(250, 480-80, 80, 0, Math.PI);
+ctx.closePath();
 ctx.fillStyle = "#f86e7d"; //lighter pink
 ctx.fill();
 
 //tongue, upper rectangular, rounded corners
 ctx.beginPath();
-ctx.moveTo(250 -80, 400); //start point
+ctx.moveTo(250 -80, 400);
 ctx.lineTo(250 -80, 320 +10);
 ctx.arcTo(250 -80, 320, 250-80 +10, 320, 10); 
-//point where angle is made ~ end point
-ctx.lineTo(250+80 -10, 320); //end point
+ctx.lineTo(250+80 -10, 320);
 ctx.arcTo(250+80, 320, 250+80, 320+10, 10);
 ctx.lineTo(250+80, 400);
 ctx.closePath();
@@ -86,6 +68,5 @@ ctx.moveTo(250-10, 320);
 ctx.lineTo(250+10, 320); 
 ctx.lineTo(250, 450);
 ctx.closePath();
-//ctx.lineTo(250-10, 320);
 ctx.fillStyle = "#e3586c";//darker pink
 ctx.fill();
