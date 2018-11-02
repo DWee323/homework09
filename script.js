2452e91eb00d1f818a9dff3548a23737a26c8987
Diane Wee <hwee1@id.iit.edu>
2018-11-02 05:55:36 -0500

    started drawing

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//https://github.com/karbassi/idx-528/tree/master/week-09/assignment
+
 /*
 The following tasks do not need to be done in order. I will review the final file.
 
@@ -13,3 +15,19 @@ The following code will be in your script.js file.
 
 Using the Canvas API, draw the following image.
 */
+
+
+let canvas = document.querySelector("#emoji");
+let ctx = canvas.getContext("2d");
+
+const circle = (x,y,radius) => {
+    ctx.arc(x,y,radius,0, 2*Math.PI);
+};
+
+//face outline
+ctx.beginPath();
+circle(250, 250, 230);
+ctx.fillStyle = "#ffde67";
+ctx.fill();
+
+//eyes
