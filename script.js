29fdba6cff9ba57399d38e1b1d7e68bacfd442ce
Diane Wee <hwee1@id.iit.edu>
2018-11-02 07:20:55 -0500

    upto mouth

--- script.js
+++ script.js
@@ -40,12 +40,30 @@ circle(250 +95, 250 -60, 35);
 ctx.fillStyle = "#664d23";
 ctx.fill();
 
-// mouth, goddamnit
-//CanvasRenderingContext2D.arcTo(): adds a circular arc to the current path. It is commonly used for making rounded corners.
-//ctx.arcTo(x1, y1, x2, y2, radius); ()
-//ctx.arc(x,y,radius,strtAngle,endAngle,(antiClockwise))
+//mouth
+//ctx.arcTo(x1, y1, x2, y2, radius); -> adds a circular arc to the current path. It is commonly used for making rounded corners.
+
+/*
 ctx.beginPath();
-ctx.moveTo(150, 20);
-ctx.arcTo(150,100, 50,20, 30);
-ctx.lineTo(50, 20);
+ctx.moveTo(x1, y1); //start point
+ctx.arcTo(x2, y2, x3, y3, radius); //point where angle is made ~ end point
+ctx.lineTo(x3, y3); //end point
 ctx.stroke();
+*/
+
+//ctx.arc(x,y,radius,strtAngle,endAngle,(antiClockwise))
+
+ctx.beginPath();
+ctx.arc(250, 250 +30, 150, 0, Math.PI);//line is there, but invisible
+ctx.closePath(); //no closePath with rounding corner?
+ctx.fillStyle = "#664d23";
+ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(250 -150, 250 +30); //start point
+ctx.arcTo(250 -150, (250+30) -10, (250-150) +10, (250+30) -10, 10); //point where angle is made ~ end point
+ctx.lineTo(250+150 -10, (250+30) -10); //end point
+ctx.arcTo(250+150, (250+30) -10, (250+150), (250+30) +10, 10);
+ctx.closePath();
+ctx.fillStyle = "#664d23";
+ctx.fill();
